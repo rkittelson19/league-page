dc3e803777ec110ffc6d06daaea14044d4102725
rkittelson19 <91913911+rkittelson19@users.noreply.github.com>
2024-06-05 14:10:53 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "1049051419367596032"; // your league ID
 export const leagueName = "Big Boi League"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 60; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
